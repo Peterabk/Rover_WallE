2d0c5e1e1d9603f1f46b2d1f1191fa7eea6e230e
Peterabk <peter.abk@outlook.com>
2025-02-13 18:05:01 -0500

    dual ekf working on no simulation time

--- wall_e_ws/src/wall_e_localization/launch/gps_waypoint_follower.launch.py
+++ wall_e_ws/src/wall_e_localization/launch/gps_waypoint_follower.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
             os.path.join(bringup_dir, "launch", "navigation_launch.py")
         ),
         launch_arguments={
-            "use_sim_time": "True",
+            "use_sim_time": "False",
             "params_file": configured_params,
             "autostart": "True",
         }.items(),
@@ -73,7 +73,7 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # simulator launch
-    ld.add_action(gazebo_cmd)
+    #ld.add_action(gazebo_cmd)
 
     # robot localization launch
     ld.add_action(robot_localization_cmd)
